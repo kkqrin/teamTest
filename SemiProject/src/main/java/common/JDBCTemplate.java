d00f2476db6bc66f13dbbdf8c463c285f46c430e
Ahn-Noah <eoekdl3@gmail.com>
2023-02-28 11:56:04 +0900

    JDBCTemplate 생성 및 기초 설정
    JDBCTemplate 생성 및 DB정보 입력

---
+++ SemiProject/src/main/java/common/JDBCTemplate.java
@@ -0,0 +1,93 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTemplate {
+	// Connection 생성 후 리턴
+	public static Connection getConnection() {
+		Connection conn = null;
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.10.18:1521:xe", "webserver", "1234");
+			// 자동으로 commit 되는 기능을 비활성화
+
+			conn.setAutoCommit(false);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	// commit 처리
+	public static void commit(Connection conn) {
+		try {
+			// conn.isClosed() : 커넥션이 닫혀있는지 확인
+			// 커넥션이 정상인 경우에만 commit
+			if (conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// rollback 처리
+	public static void rollback(Connection conn) {
+		try {
+			// conn.isClosed() : 커넥션이 닫혀있는지 확인
+			// 커넥션이 정상인 경우에만 commit
+			if (conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// 자원 반환(conn , stmt, rset)
+	public static void close(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(Statement stmt) {
+
+		try {
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rset) {
+		try {
+			if (rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
