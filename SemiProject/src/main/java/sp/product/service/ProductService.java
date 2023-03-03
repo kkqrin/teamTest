52fca6c55140aa64e148bf368565351e017a9cfa
kkqrin <126125426+kkqrin@users.noreply.github.com>
2023-03-03 14:30:05 +0900

    헤더 검색창 클래스명 수정

--- SemiProject/src/main/java/sp/product/service/ProductService.java
+++ SemiProject/src/main/java/sp/product/service/ProductService.java
@@ -1,9 +1,11 @@
 package sp.product.service;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import common.JDBCTemplate;
 import sp.product.dao.ProductDao;
+import sp.product.vo.Category;
 
 public class ProductService {
 	private ProductDao dao;
@@ -17,5 +19,11 @@ public class ProductService {
 		Connection conn = JDBCTemplate.getConnection();
 		int result = dao.reserveProduct(conn,productNo);
 		return 0;
+	}
+
+	public ArrayList<Category> selectOneSubCategory() {
+		Connection conn = JDBCTemplate.getConnection();
+		ArrayList<Category> list = dao.selectOneSubCategory(conn);
+		return null;
 	}
 }
