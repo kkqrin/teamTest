cb0c3c31b7d17fa509fb1c36ccd0bb02489c1e28
Ahn-Noah <eoekdl3@gmail.com>
2023-03-07 14:49:01 +0900

    신고자 커밋

---
+++ SemiProject/src/main/java/sp/report/controller/FindReportUserServlet.java
@@ -0,0 +1,42 @@
+package sp.report.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FineReportUserServlet
+ */
+@WebServlet(name = "FindReportUser", urlPatterns = { "/findReportUser.do" })
+public class FindReportUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FindReportUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String memberId = request.getParameter("memberId");
+		System.out.println(memberId);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
